32e72811fa4851c11610314521ae4f8c3666708c
henriquebussi <henriquebussidossantos@gmail.com>
2024-06-18 17:11:30 -0300

    hi

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-let input = document.getElementById("inputbox")
-let add =document.getElementById("add")
-let div = document.createElement("div")
+let input = document.getElementById("inputbox");
+let add = document.getElementById("add");
+let main = document.getElementById("main")
+let count = 0 
 
-add.addEventListener("click", () =>{
+add.addEventListener("click", () => {
     if(input.value == ''){
-        window.alert("digitr algo")
+        window.alert("Digite algo");
     } else{
         console.log(input.value);
-        let inp = document.createTextNode(input)
-        div.appendChild(inp)
+        count++
+
+        let div = document.createElement("div"); // Criar a div dentro do evento de clique
+        div.classList.add("item" + count)
+        div.classList.add("item")
+        let inp = document.createTextNode(input.value); // Criar o nó de texto a partir do valor do input
+        div.appendChild(inp);
+        main.appendChild(div); // Adicionar a nova div ao documento
+        input.value = ''; // Limpar o valor do input após adicionar
     }
-})
+});
+
+main.addEventListener("click", (event) => {
+    if(event.target && event.target.classList.contains("item")) {
+        main.removeChild(event.target); // Remove o elemento clicado
+    }
+});
