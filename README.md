9e52364ed8bf8f97a4b5afe316eb3fd344609d75
henriquebussi <henriquebussidossantos@gmail.com>
2024-06-19 23:32:24 -0300

    rightclick and more

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ToDoList
+
+**ToDoList** como o nome diz é uma lista de tarefas interativa, onde você pode finalizar e excluir tarefas de maneira fácil e rápido
+
+**Botão Direito** - Finalizar Tarefa
+**Botão Esquerdo** - Excluir Tarefa
--- script.js
+++ script.js
@@ -3,17 +3,17 @@ let add = document.getElementById("add");
 let main = document.getElementById("main")
 let count = 0 
 
-add.addEventListener("click", () => {
+add.addEventListener("click", () => { // Quando algo clickar, acontecera:
     if(input.value == ''){
-        window.alert("Digite algo");
+        window.alert("Digite algo"); // se n tiver nada, da erro
     } else{
         console.log(input.value);
         count++
 
-        let div = document.createElement("div"); // Criar a div dentro do evento de clique
+        let div = document.createElement("div"); // Cria a div 
         div.classList.add("item" + count)
         div.classList.add("item")
-        let inp = document.createTextNode(input.value); // Criar o nó de texto a partir do valor do input
+        let inp = document.createTextNode(input.value); // Criar o "nó de texto" a partir do valor do input
         div.appendChild(inp);
         main.appendChild(div); // Adicionar a nova div ao documento
         input.value = ''; // Limpar o valor do input após adicionar
@@ -25,3 +25,18 @@ main.addEventListener("click", (event) => {
         main.removeChild(event.target); // Remove o elemento clicado
     }
 });
+
+main.addEventListener("contextmenu", (e) => {
+    if(e.target && e.target.classList.contains("item")){
+        if(e.target.style.textDecoration == 'line-through'){
+            e.preventDefault()
+            e.target.style.textDecoration = 'none'
+            e.target.style.color = ""
+
+        } else{
+            e.preventDefault()
+            e.target.style.textDecoration = 'line-through'
+            e.target.style.color = "red"
+    } 
+    }
+})
